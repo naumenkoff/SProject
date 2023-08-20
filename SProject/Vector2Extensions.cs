05e4a8fe8766f58282cacb7524f4754cfd25a0f3
naumenkoff <naumenkoff1337@gmail.com>
2023-08-20 16:41:35 +0300

    Added Vector2 extensions

---
+++ SProject/Vector2Extensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace SProject;
+
+public static class Vector2Extensions
+{
+    public static Vector2 Floor(this Vector2 vector2)
+    {
+        return new Vector2(float.Truncate(vector2.X), float.Truncate(vector2.Y));
+    }
+
+    public static Vector2 Lerp(this Vector2 vector2, double value)
+    {
+        return vector2 * (float) double.Clamp(value, 0.0, 1.0);
+    }
+
+    public static Vector2 Lerp(this Vector2 vector2, float value)
+    {
+        return vector2 * float.Clamp(value, 0f, 1f);
+    }
+
+    public static Vector2 ScaleX(this Vector2 vector2, float xValue)
+    {
+        return vector2 with
+        {
+            X = vector2.X * xValue
+        };
+    }
+
+    public static Vector2 ScaleY(this Vector2 vector2, float yValue)
+    {
+        return vector2 with
+        {
+            Y = vector2.Y * yValue
+        };
+    }
+
+    public static Vector2 Scale(this Vector2 vector2, float xValue, float yValue)
+    {
+        return new Vector2(vector2.X * xValue, vector2.Y * yValue);
+    }
+}
