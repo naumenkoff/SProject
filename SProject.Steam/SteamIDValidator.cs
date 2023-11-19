8475e85ba7e87e2d6f8b89a68f90911857b69cc4
naumenkoff <naumenkoff1337@gmail.com>
2023-11-19 12:44:34 +0300

    Fixed that SteamIDValidator wasn't static

--- SProject.Steam/SteamIDValidator.cs
+++ SProject.Steam/SteamIDValidator.cs
@@ -1,23 +1,23 @@
 ﻿namespace SProject.Steam;
 
-public class SteamIDValidator
+public static class SteamIDValidator
 {
-    public bool IsSteamID64(string id)
+    public static bool IsSteamID64(string id)
     {
         return long.TryParse(id, out var steamID64) && IsSteamID64(steamID64);
     }
 
-    public bool IsSteamID64(long id)
+    public static bool IsSteamID64(long id)
     {
         return id is >= WellKnownConstants.SteamID64Offset and <= WellKnownConstants.SteamID64MaximumValue;
     }
 
-    public bool IsSteamID32(string id)
+    public static bool IsSteamID32(string id)
     {
         return uint.TryParse(id, out var steamID32) && IsSteamID32(steamID32);
     }
 
-    public bool IsSteamID32(uint steamID32)
+    public static bool IsSteamID32(uint steamID32)
     {
         return steamID32 != 0;
     }
