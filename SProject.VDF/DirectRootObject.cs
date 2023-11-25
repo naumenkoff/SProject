4a93a60b87aa7cf843099e953d0b82bccbfb764b
naumenkoff <naumenkoff1337@gmail.com>
2023-11-25 03:22:52 +0300

    added default indexer

--- SProject.VDF/DirectRootObject.cs
+++ SProject.VDF/DirectRootObject.cs
@@ -7,7 +7,7 @@ public class DirectRootObject : IRootObject<IRootObject, IValueObject>
     public required string? Key { get; init; }
     public Dictionary<string, IValueObject> ValueObjects { get; } = new Dictionary<string, IValueObject>();
     public Dictionary<string, IRootObject> RootObjects { get; } = new Dictionary<string, IRootObject>();
-
+    IValueObject IRootObject.this[string key] => this[key];
     public IRootObject this[string key] => RootObjects[key];
 
     public IValueObject Get(string key)
--- SProject.Vdf.Abstractions/IRootObject.cs
+++ SProject.Vdf.Abstractions/IRootObject.cs
@@ -2,11 +2,12 @@ namespace SProject.Vdf.Abstractions;
 
 public interface IRootObject<out TDirect, out TIndirect> : IRootObject where TDirect : IValueObject where TIndirect : IValueObject
 {
-    TDirect this[string key] { get; }
+    new TDirect this[string key] { get; }
     TIndirect Get(string key);
 }
 
 public interface IRootObject : IValueObject {
     Dictionary<string, IValueObject> ValueObjects { get; }
     Dictionary<string, IRootObject> RootObjects { get; }
+    IValueObject this[string key] { get; }
 }
