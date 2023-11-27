253358d437a55ceeb1c9ae5a9acf20966867bbaf
naumenkoff <naumenkoff1337@gmail.com>
2023-11-27 23:39:44 +0300

    Changed Cast<T>, GetValue<T> and As<T> methods

--- SProject.VDF/ValueObjectExtensions.cs
+++ SProject.VDF/ValueObjectExtensions.cs
@@ -6,36 +6,31 @@ public static class ValueObjectExtensions
 {
     public static T? As<T>(this IValueObject? valueObject) where T : IValueObject
     {
-        return valueObject is null ? default : (T) valueObject;
+        return (T?) valueObject ?? default;
     }
 
-    public static T GetValue<T>(this IValueObject abstraction, string key)
+    public static T GetValue<T>(this IValueObject value, string key)
     {
-        switch (abstraction)
+        return value switch
         {
-            case ValueObject valueObject:
-            {
-                return valueObject.Key == key ? Cast<T>(valueObject.Value) : throw new KeyNotFoundException($"{key} != {valueObject.Key}");
-            }
-            case IRootObject rootObject:
-            {
-                var value = rootObject.GetValueObject<ValueObject>(key);
-                return Cast<T>(value?.Value);
-            }
-            default: { throw new NotSupportedException($"{abstraction.GetType()} doesn't support getting value"); }
-        }
-    }
-
-    private static T Cast<T>(string? content)
-    {
-        ArgumentException.ThrowIfNullOrEmpty(content);
-        return typeof(T) switch
-        {
-            { } type when type == typeof(int) => (T) (object) Convert.ToInt32(content),
-            { } type when type == typeof(long) => (T) (object) Convert.ToInt64(content),
-            { } type when type == typeof(bool) => (T) (object) (content == "1"),
-            { } type when type == typeof(string) => (T) (object) content,
-            _ => throw new InvalidCastException($"Casting to {typeof(T)} isn't supported.")
+            ValueObject valueObject => Cast<T>(valueObject.Value),
+            IRootObject rootObject => Cast<T>(rootObject.GetValueObject<ValueObject>(key)?.Value!),
+            _ => throw new InvalidOperationException($"Unsupported type: {value.GetType()}")
         };
+    }
+
+    private static T Cast<T>(string value)
+    {
+        var type = typeof(T);
+        try
+        {
+            if (type == typeof(int)) return (T) (object) int.Parse(value);
+            if (type == typeof(uint)) return (T) (object) uint.Parse(value);
+            if (type == typeof(long)) return (T) (object) long.Parse(value);
+            if (type == typeof(ulong)) return (T) (object) ulong.Parse(value);
+            if (type == typeof(string)) return (T) (object) value;
+            throw new InvalidOperationException($"Unsupported type: {type}");
+        }
+        catch (Exception exception) { throw new InvalidCastException($"Unable to cast '{value}' to {type}", exception); }
     }
 }
