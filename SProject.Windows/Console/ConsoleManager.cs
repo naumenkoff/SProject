b2f5fb83d13de21b9c4044f8802944a44c1cdbdf
naumenkoff <naumenkoff1337@gmail.com>
2023-12-02 16:53:27 +0300

    Added new 'Windows' package

---
+++ SProject.Windows/Console/ConsoleManager.cs
@@ -0,0 +1,60 @@
+﻿using SProject.Windows.Console.Internal;
+
+namespace SProject.Windows.Console;
+
+// CA1822: Mark members as static
+#pragma warning disable CA1822
+
+public sealed class ConsoleManager : IDisposable
+{
+    private static StreamWriter? _stdErr;
+    private static StreamWriter? _stdOut;
+
+    public bool HasExternalConsole { get; } = ConsoleInvoke.AttachConsole(0x0FFFFFFFF);
+
+    public void Dispose()
+    {
+        _stdErr?.Dispose();
+        _stdOut?.Dispose();
+    }
+
+    public void Initialize()
+    {
+        if (HasExternalConsole || HasConsoleWindow(out _)) return;
+
+        ConsoleInvoke.AllocConsole();
+        HideConsole();
+        RedirectConsoleOutput();
+    }
+
+    public void ShowConsole()
+    {
+        if (HasConsoleWindow(out var handle)) ConsoleInvoke.ShowWindow(handle, (int) ShowWindow.Show);
+    }
+
+    public void HideConsole()
+    {
+        if (HasConsoleWindow(out var handle)) ConsoleInvoke.ShowWindow(handle, (int) ShowWindow.Hide);
+    }
+
+    private static void RedirectConsoleOutput()
+    {
+        _stdOut = new StreamWriter(System.Console.OpenStandardError())
+        {
+            AutoFlush = true
+        };
+        _stdErr = new StreamWriter(System.Console.OpenStandardOutput())
+        {
+            AutoFlush = true
+        };
+
+        System.Console.SetOut(_stdOut);
+        System.Console.SetError(_stdErr);
+    }
+
+    private static bool HasConsoleWindow(out IntPtr handle)
+    {
+        handle = ConsoleInvoke.GetConsoleWindow();
+        return handle != IntPtr.Zero;
+    }
+}
---
+++ SProject.Windows/Console/Internal/ShowWindow.cs
@@ -0,0 +1,17 @@
+﻿namespace SProject.Windows.Console.Internal;
+
+/// <summary>
+///     https://learn.microsoft.com/ru-ru/windows/win32/api/winuser/nf-winuser-showwindow
+/// </summary>
+internal enum ShowWindow
+{
+    /// <summary>
+    ///     Hides the window and activates another window.
+    /// </summary>
+    Hide = 0,
+
+    /// <summary>
+    ///     Activates the window and displays it in its current size and position.
+    /// </summary>
+    Show = 5
+}
