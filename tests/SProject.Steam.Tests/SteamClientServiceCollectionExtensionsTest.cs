46b4274798e7fa4279baba2b858e64a1ebcd01d0
naumenkoff <naumenkoff1337@gmail.com>
2024-02-24 00:38:51 +0300

    Added SteamClientServiceCollectionExtensions tests

---
+++ tests/SProject.Steam.Tests/SteamClientServiceCollectionExtensionsTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SProject.Steam.Tests;
+
+[TestFixture]
+[TestOf(typeof(SteamClientServiceCollectionExtensions))]
+public class SteamClientServiceCollectionExtensionsTest
+{
+    [Test]
+    public void AddSteamClient_ReturnsServiceCollection_WithAddedDependencies()
+    {
+        // Arrange
+        var serviceCollection = new ServiceCollection();
+
+        // Act
+        serviceCollection.AddSteamClient();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.That(serviceCollection, Has.One.Matches<ServiceDescriptor>(x => x.ServiceType == typeof(ISteamClientFinder)));
+            Assert.That(serviceCollection,
+                Has.One.Matches<ServiceDescriptor>(x => x.ServiceType == typeof(ISteamInstallPathResolver<SteamPathNode>)));
+        });
+    }
+}
